ac615dc18c204349383364c335a10c46427a492b
Stephen Kairu <skairu09@gmail.com>
2022-07-19 15:15:52 +0300

    Add donut chart using javascript

---
+++ script.js
@@ -0,0 +1,43 @@
+// google.charts.load("current", {packages:["corechart"]});
+//       google.charts.setOnLoadCallback(drawChart);
+//       function drawChart() {
+//         var data = google.visualization.arrayToDataTable([
+//           ['Task', 'Hours per Day'],
+//           ['Work',     11],
+//           ['Eat',      2],
+//           ['Commute',  2],
+//           ['Watch TV', 2],
+//           ['Sleep',    7]
+//         ]);
+
+//         var options = {
+//           title: 'My Daily Activities',
+//           pieHole: 0.4,
+//         };
+
+//         var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+//         chart.draw(data, options);
+//       }
+
+
+google.charts.load("current", { packages: ["corechart"] });
+google.charts.setOnLoadCallback(drawChart);
+function drawChart() {
+    var data = google.visualization.arrayToDataTable([
+        ['Trips', ''],
+        ['Completed', 20],
+        ['NotCompleted', 0],
+        
+    ]);
+
+    var options = {
+        title: 'Daily Trips',
+        pieHole: 0.6,
+        
+    };
+
+    
+
+    var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+    chart.draw(data, options);
+}
