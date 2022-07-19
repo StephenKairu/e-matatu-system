f1175db632ee4326cc20a0eec757c0697fb226e7
G-vans <jevans.otieno@student.moringaschool.com>
2022-07-19 16:12:59 +0300

    add script line graph

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,8 @@ anychart.onDocumentReady(function() {
 
     // set the data
     var data = [
-        {x: "Nissan", value: 210},
-        {x: "Mini Bus", value: 140},
+        {x: "Nissan", value: 2000},
+        {x: "Mini Bus", value: 2400},
     ];
   
     // create the chart
@@ -21,3 +21,54 @@ anychart.onDocumentReady(function() {
     chart.draw();
   
   });
+
+  //vehicles doughnut chart
+  var xValues = ["Active", "Repair", "Impounded"];
+var yValues = [138, 30, 12];
+var barColors = [
+  "#b91d47",
+  "#00aba9",
+  "#1e7145"
+];
+
+new Chart("myChart", {
+  type: "doughnut",
+  data: {
+    labels: xValues,
+    datasets: [{
+      backgroundColor: barColors,
+      data: yValues
+    }]
+  },
+  options: {
+    title: {
+      display: true,
+      text: "Vehicle status"
+    }
+  }
+});
+
+//revenue graphs
+var xValues2 = ["Mon","Tue","Wed","Thur","Fri","Sat","Sun"];
+
+new Chart("myChart2", {
+  type: "line",
+  data: {
+    labels: xValues2,
+    datasets: [{ 
+      data: [6600,5500,5000,5000,4700,4500,3000],
+      borderColor: "red",
+      fill: false
+    }, { 
+      data: [3300,3000,3000,4500,5000,4000,1000],
+      borderColor: "blue",
+      fill: false
+    }]
+  },
+  options: {
+    title: {
+      display: true,
+      text: "Weekly Revenues in '000"
+    }
+  }
+});
