5506ddbf52870ab79a304c0aeb921307d5da7f01
Stephen Kairu <skairu09@gmail.com>
2022-07-21 15:41:23 +0300

    Update donut chart

--- script.js
+++ script.js
@@ -1,46 +1,21 @@
-// google.charts.load("current", {packages:["corechart"]});
-//       google.charts.setOnLoadCallback(drawChart);
-//       function drawChart() {
-//         var data = google.visualization.arrayToDataTable([
-//           ['Task', 'Hours per Day'],
-//           ['Work',     11],
-//           ['Eat',      2],
-//           ['Commute',  2],
-//           ['Watch TV', 2],
-//           ['Sleep',    7]
-//         ]);
 
-//         var options = {
-//           title: 'My Daily Activities',
-//           pieHole: 0.4,
-//         };
+google.charts.load("current", {packages:["corechart"]});
+      google.charts.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+            ['Trips', 'Number'],
+          ['Complete',     15],
+          ['Not Completed',      5],
+        ]);
 
-//         var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
-//         chart.draw(data, options);
-//       }
+        var options = {
+          title: 'Today Trips',
+          pieHole: 0.4,
+        };
 
-
-google.charts.load("current", { packages: ["corechart"] });
-google.charts.setOnLoadCallback(drawChart);
-function drawChart() {
-    var data = google.visualization.arrayToDataTable([
-        ['Trips', '', { role: 'style'}],
-        ['Completed', 15, 'green'],
-        ['NotCompleted',5, "black"],
-        
-    ]);
-
-    var  options = {
-        title: 'Daily Trips',
-        pieHole: 0.6,
-        style :['black']
-    };
-
-    
-
-    var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
-    chart.draw(data, options);
-}
+        var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+        chart.draw(data, options);
+      }
 
 var arrDepart=new Array();
 var arrRoute=new Array();
@@ -49,7 +24,7 @@ var arrTrips=new Array();
  
 function savedata(){
     var name = document.getElementById('name').value;
-    var trip = document.getElementById('route').value;
+    var route = document.getElementById('route').value;
     var depart = document.getElementById('depart').value;
     var trip = document.getElementById('trip').value;
     arrDepart[arrDepart.length]=depart;
@@ -62,10 +37,10 @@ function savedata(){
 function displayData() 
 {
   var content="<b>Data Entered by Stage Manager :</b><br>";
-  content+= [...arrNames]+"</br>";
-  content+= [...arrRoute]+"</br>";
-  content+=[...arrTrips]+"</br>";
-  content+=[...arrDepart]+"</br>";
+  content+= [...arrNames]+"</br>"+"</br>";
+  content+= [...arrRoute]+"</br>"+"</br>";
+  content+=[...arrTrips]+"</br>"+"</br>";
+  content+=[...arrDepart]+"</br>"+"</br>";
   
  
   document.getElementById('display').innerHTML = content;
